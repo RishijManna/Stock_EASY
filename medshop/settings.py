18a539b06bcbbf1270977dfc5b641787811a0ed8
Rishij <rishijmanna2018@gmail.com>
2025-10-30 19:56:51 +0530

    Enable humanize app and fix reports template error

--- medshop/settings.py
+++ medshop/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-not-secret-change-me")
 
 # In Render you should set DEBUG=False (env var)
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
 ALLOWED_HOSTS = [
     "localhost",
@@ -36,8 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
-    # Your apps
+    "django.contrib.humanize",   # ← add this
     "inventory",
     "accounts",
     "reports",
